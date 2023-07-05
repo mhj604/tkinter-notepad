ee810a4b520387a7a2be903ffbca37bee5842623
mhj604 <mhj604@gmail.com>
2023-07-05 12:55:29 +0900

    버튼 활성화

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from tkinter import *
 from tkinter.filedialog import *
 
+def new_file():
+    text_area.delete(1.0, END)
+
+def save_file():
+    f = asksaveasfile(mode = "w", defaultextension=".txt",filetypes= [('Text files','.txt')])
+    text_save = str(text_area.get(1.0,END))
+    f.write(text_save)
+    f.close()
+
+def maker():
+    help_view = Toplevel(window)
+    help_view.geometry("300x50")
+    help_view.title("만든이")
+    lb = Label(help_view, text = "정민형이 만든 메모장입니다.")
+    lb.pack()
+
 window = Tk()
 window.title("Notepad")
-window.geometry("400X400")
+window.geometry("400x400")
 window.resizable(False, False)
 
 menu = Menu(window)
@@ -15,7 +31,7 @@ menu_1.add_command(label="종료", command=window.destroy)
 menu.add_cascade(label="파일", menu=menu_1)
 
 menu_2 = Menu(menu, tearoff=0)
-menu_2.add_command(label="만든이")
+menu_2.add_command(label="만든이", command=maker)
 menu.add_cascade(label="만든이", menu=menu_2)
 
 text_area = Text(window)
